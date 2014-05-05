22d13ec2abfd22cff976b53596441f6e6de454ff
Matt Rubin <opensource@mattrubin.me>
2014-05-05 00:11:38 -0400

    Simple scan of files in a hard-coded directory

--- Spiderweb/SWAppDelegate.m
+++ Spiderweb/SWAppDelegate.m
@@ -12,7 +12,54 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    NSURL *dirURL = [NSURL fileURLWithPath:[NSHomeDirectory() stringByAppendingPathComponent:@"spiderweb"]];
+    [self scanDirectoryAtURL:dirURL];
+}
+
+- (void)scanDirectoryAtURL:(NSURL *)directoryToScan
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    NSDirectoryEnumerator *dirEnumerator = [fileManager enumeratorAtURL:directoryToScan
+                                             includingPropertiesForKeys:@[NSURLNameKey,
+                                                                          NSURLIsRegularFileKey,
+                                                                          NSURLIsReadableKey]
+                                                                options:NSDirectoryEnumerationSkipsHiddenFiles |
+                                                                        NSDirectoryEnumerationSkipsPackageDescendants |
+                                                                        NSDirectoryEnumerationSkipsSubdirectoryDescendants
+                                                           errorHandler:^BOOL(NSURL *url, NSError *error) {
+                                                               NSLog(@"ERROR: %@", error);
+                                                               return YES;
+                                                           }];
+
+    for (NSURL *theURL in dirEnumerator) {
+        NSNumber *isRegularFile;
+        [theURL getResourceValue:&isRegularFile forKey:NSURLIsRegularFileKey error:NULL];
+        if (!isRegularFile.boolValue) {
+            continue;
+        }
+
+        NSNumber *isReadable;
+        [theURL getResourceValue:&isReadable forKey:NSURLIsReadableKey error:NULL];
+        if (!isReadable.boolValue) {
+            continue;
+        }
+
+        NSString *fileName;
+        [theURL getResourceValue:&fileName forKey:NSURLNameKey error:NULL];
+        NSLog(@"%@ :", fileName);
+
+
+        NSStringEncoding stringEncoding;
+        NSError *readError;
+        NSString *fileString = [NSString stringWithContentsOfURL:theURL
+                                                    usedEncoding:&stringEncoding
+                                                           error:&readError];
+        if (readError) {
+            NSLog(@"Read error: %@", readError.localizedDescription);
+        }
+        NSLog(@"%@", fileString);
+    }
 }
 
 @end
