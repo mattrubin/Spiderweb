129ff51aed749e70827ca10fddc60ef9f334ac86
Matt Rubin <opensource@mattrubin.me>
2014-05-06 11:25:45 -0400

    Add a details form which updates with the selected node

--- Spiderweb/SWMainViewController.m
+++ Spiderweb/SWMainViewController.m
@@ -12,6 +12,10 @@
 
 @interface SWMainViewController () <NSTableViewDataSource, NSTableViewDelegate>
 
+@property (weak) IBOutlet NSTableView *tableView;
+@property (weak) IBOutlet NSTextField *titleField;
+@property (weak) IBOutlet NSTextField *uniqueIDField;
+
 @end
 
 
@@ -48,6 +52,35 @@
     }
 
     return cellView;
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification
+{
+    [self updateFieldsWithNode:self.selectedNode];
+
+}
+
+- (SWNode *)selectedNode
+{
+    NSIndexSet *selectedRows = self.tableView.selectedRowIndexes;
+
+    if (selectedRows.count == 1) {
+        NSUInteger selectedRow = selectedRows.firstIndex;
+        return self.models[selectedRow];
+    }
+
+    return nil;
+}
+
+- (void)updateFieldsWithNode:(SWNode *)node
+{
+    if (!node) {
+        self.titleField.stringValue = @"";
+        self.uniqueIDField.stringValue = @"";
+    } else {
+        self.titleField.stringValue = node.title ?: @"";
+        self.uniqueIDField.stringValue = node.uniqueID.UUIDString;
+    }
 }
 
 @end
